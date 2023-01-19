e25c2215a5acdf62040b32e7a2357153a39e1ef6
Micha K <micha@kuehn.io>
2023-01-19 13:00:54 +0100

    change default subreddit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+	let subreddit = 'apple';
 	import SubmissionPreviewList from '$lib/submission-preview-list.svelte';
-	const subreddit = 'teslamotors';
 </script>
 
 <h1 class="border ">Posts in /r/{subreddit}</h1>
